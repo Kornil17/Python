62ccbc2b7e268daa193e1625b9ffa88412ce751c
Dmitriy K <d.kornilov@r77.center-inform.ru>
2023-10-13 18:00:27 +0300

    done read kafka for domain service

--- UTM/Domain-info/send_files_kafka.py
+++ UTM/Domain-info/send_files_kafka.py
@@ -1,19 +1,46 @@
 import logging
-import kafka
+# from kafka import KafkaConsumer
+# /tmp/python_venv_dm
 from confluent_kafka import Consumer
 class Kafka:
-    def __init__(self):
-        self.connect = self.connection()
+    def __init__(self, topic):
         self.contur = 'gitlab-ci.ru:9092'
-        self.topic = ['svs-inspector']
-        self.consumer = self.connect.subscribe(self.topic)
+        # self.contur = '10.10.4.28:9092'
+        self.topic = topic
+        self.connect = self.connection()
+        self.consumer = self.connect
 
     def connection(self):
-        result_connection = kafka.KafkaConsumer(bootstrap_servers=self.contur,
-                            group_id='group_id',
-                            consumer_timeout_ms=60000,
-                            auto_offset_reset='earliest',
-                            enable_auto_commit=False)
+        logging.debug('start connection function')
+        result_connection = Consumer({'bootstrap.servers':self.contur, 'group.id':'group', 'auto.offset.reset':'earliest','enable.auto.commit':False})
+        logging.debug('exit from connection function')
         return result_connection
 
-# new
+    def write_message(self):
+        pass
+
+    def read_message(self):
+        logging.debug('start kafka read function')
+        try:
+            logging.debug('Start read messages from kafka')
+            while True:
+                logging.info(f'subscribe topic {self.topic}')
+                self.consumer.subscribe([self.topic])
+                msg = self.consumer.poll(1.0)
+                if msg is None:
+                    continue
+                if msg.error():
+                    print(f"Consumer error: {msg.error()}")
+                logging.info('print all messages from topic')
+                print(f"Received message: {msg.value().decode('utf-8')}")
+                # break
+        except KeyboardInterrupt:
+            logging.warning("Aborted by user...")
+        except Exception as error:
+            logging.error(f'Service error: {error}')
+        finally:
+            self.consumer.close()
+            logging.info('Close the kafka connection')
+kafkin = Kafka(input('Write topic\n'))
+logging.info(f'Get topic named as: {kafkin.topic}')
+kafkin.read_message()
